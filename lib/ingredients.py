320a0b179e848294b19eb1bedb088d61c9509f38
unknown <er.utkarshtrivedi@gmail.com>
2020-10-06 19:59:37 +0530

    Added a new ingrediants class

---
+++ lib/ingredients.py
@@ -0,0 +1,7 @@
+class ingredients():
+    def __init__(self,name,kind,calories,color):
+        self.name = name
+        self.kind = kind
+        self.calories = calories
+        self.color = color
+        
