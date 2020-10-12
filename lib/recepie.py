795cd07fd8dd55bdfcbed9f05ec6ae7904a8015a
unknown <er.utkarshtrivedi@gmail.com>
2020-10-12 13:55:27 +0530

    Working on unit measurement and coversion

--- lib/recepie.py
+++ lib/recepie.py
@@ -1,9 +1,8 @@
 #this class should be able to handle all ingredients for recepie 
-class ingredients():
+class recepie():
     def __init__(self,name,kind,calories,color):
         self.name = name
         self.kind = kind
         self.calories = calories
         self.color = color
-        
         
