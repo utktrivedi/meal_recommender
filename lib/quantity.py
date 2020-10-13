a4a0fc43bede0acf3988a34a567b8159252c6af7
unknown <er.utkarshtrivedi@gmail.com>
2020-10-13 21:48:36 +0530

    added method to units + other stuff

--- lib/quantity.py
+++ lib/quantity.py
@@ -1,30 +1,10 @@
-import enum
-
-#Making a class for all the units of Measurement
-#source: https://en.wikibooks.org/wiki/Cookbook:Units_of_measurement
-
-#ToDo : Add type of measurement (Weight, Volume)
-#ToDo : How to incorportate t, tsp. , tablesoop etc as same inputs
-
-class unit(enum.Enum):
-    tsp=1  #teaspoon (also t or tsp.)
-    tbl=2  #tablespoon (also T, tbl., tbs., or tbsp.)
-    fl_oz=3  #fluid ounce (also fl oz)
-    gill=4  #gill (about 1/2 cup)
-    cup=5 #cup (also c)
-    pt=6  #pint (also p, pt, or fl pt - Specify Imperial or US)
-    qt=7  #quart (also q, qt, or fl qt - Specify Imperial or US)
-    gal=8  #gallon (also g or gal - Specify Imperial or US)
-    ml=9  #ml, also milliliter, millilitre, cc (and mL only in the US, Canada and Australia).
-    l=10  #l, also liter, litre, (and L only in the US, Canada and Australia).
-    dl=11  #dl, also deciliter, decilitre (and dL only in the US, Canada and Australia).
-    lb=12 #pound (also lb or #)
-    oz=13 #ounce (also oz)
-    mg=14 #mg (also milligram or milligramme)
-    g=15 #g (also gram or gramme)
-    kg=16 #kg (also kilogram or kilogramme)
+import unit 
 
 class quantity():
-    def __init__(self,amount,unit):
+    def __init__(self,amount:int,unit_:str):
         self.amount=amount
-        self.unit=unit      
+        self.unit=unit_
+
+    def from_string(self, )
+
+    def convert_to()      
---
+++ lib/unit.py
@@ -0,0 +1,56 @@
+#ToDo : Add type of measurement (Weight, Volume)
+#ToDo : How to incorportate t, tsp. , tablesoop etc as same inputs
+
+class unit():
+    """
+    Generic class to define a measurement unit. It contains following attributes
+    Name : Name of the unit e.g. gms
+    Property_ : Fundamental characterstic being measured e.g. Weight or Volume
+    si_conv_factor : Used to convert units to SI unit e.g. gms = 0.001 kg -> si_conv_factor=0.001 (cannot handle temprature units)
+    Aliases : List of other names the unit might be known by e.g. grams, gram, gramme
+    """
+    def __init__(self, name:str, property_:str, si_conv_factor:float, aliases=None):
+        self.name=name              #name of the unit eg gram
+        self.property=property_     #Property being measured e.g. Weight/ Volume
+        self.si_conv_factor= si_conv_factor
+        if aliases is None:
+            self.aliases = []
+        else:
+            if isinstance(aliases,list):
+                self.aliases=aliases    #list of other aliases for unit e.g. g for gram
+            else:
+                Exception
+    
+    def __repr__(self):
+        return "unit('{}','{}','{}','{}')".format(self.name,self.property,self.si_conv_factor,self.aliases) 
+    
+    def add_alias(self,alias:str):
+        """Method to add alias to exitng aliases of unit, can be used to manipulate aliases during runtime"""
+        if alias not in self.aliases:
+            self.aliases.append(alias)
+    
+    def remove_alias(self, alias:str):
+        """Method to remove alias from exitng aliases, can be used to manipulate aliases during runtime"""
+        if alias in self.aliases:
+            self.aliases.remove(alias)
+    
+    def convertion_factor(self, target_unit:unit):
+        """This method return the converstion factors between units measuring same property e.g. weight, volume"""
+        if self.property == target_unit.property:
+            return self.si_conv_factor/target_unit.si_conv_factor
+        else:
+            Exception
+    
+class weight_unit(unit):
+    """This """
+    kg=unit('kg','Weight',1,['kg','kgs','kilogram',"kilogramme"])
+    si_unit = kg
+    def __init__(self,name:str,si_conv_factor:float,aliases=None):
+        super().__init__(name,"Weight",si_conv_factor, aliases)
+
+class volume_unit(unit):
+    meter_cube = unit("meter_cube","Volume", 1, ['m3','m^3','meter_cube','meter3','meter^3'])
+    si_unit = meter_cube
+    def __init__(self,name:str,si_conv_factor:float,aliases=None):
+        super().__init__(name,"Volume",si_conv_factor,aliases)
+
