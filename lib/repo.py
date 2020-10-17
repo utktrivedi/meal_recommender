3a05cc97917425f993e6b230251d74d74ae78b69
unknown <er.utkarshtrivedi@gmail.com>
2020-10-17 13:28:02 +0530

    added test notebook

--- lib/repo.py
+++ lib/repo.py
@@ -6,46 +6,46 @@ class repo():
     """This class is created to act as database for objects of classes like unit() and ingrediants()
     it has methods to add or remove items from repo, find out correct item from aliases, import and export database 
     """
-    def __init__(self,name:str,file_path:str=None,object_list=None):
+    def __init__(self,name:str,file_path:str=None,object_set=None):
         self.name = name
         if file_path is None:
             self.file_path=Path.cwd()/name
         else:
             self.file_path = Path(file_path)
 
-        if object_list is None:
-            self.object_list = []
+        if object_set is None:
+            self.object_set = set([])
         else:
-            if isinstance(object_list,list):
-                self.object_list=object_list
+            if isinstance(object_set,set):
+                self.object_set=object_set
             else:
                 Exception
     
     def __len__(self):
-        return len(self.object_list)
+        return len(self.object_set)
     
     def __repr__(self, seperator='\n'):
         """ Return random 10 records from repo"""
-        if len(self.object_list)>10:
-            return seperator.join(map(str,self.object_list[:10]))
+        if len(self.object_set)>10:
+            return seperator.join(map(str,self.object_set[:10]))
         else:
-            return seperator.join(map(str,self.object_list))
+            return seperator.join(map(str,self.object_set))
     
     @staticmethod
     def load(file_path:str):
         with open(file_path,"rb") as f:
-            repo=pickle.load(f)
-        return repo
+            payload=pickle.load(f)
+        return payload
     
     def add_record(self,new_record:any):
         """ Method to add new record to repo"""
-        self.object_list.append(new_record)
+        self.object_set.add(new_record)
         self.save()
     
     def remove_record(self,old_record:any):
         """ Method to remove a record from repo"""
-        if (old_record in self.object_list):
-            self.object_list.remove(old_record)
+        if (old_record in self.object_set):
+            self.object_set.remove(old_record)
         self.save()
     
     def save(self):
@@ -60,6 +60,6 @@ class repo():
 
     def find_record(self,search_word:str):
         """Method to handle various aliases of unit, ingredients etc and returns the standard record back"""
-        for record in self.object_list:
+        for record in self.object_set:
             if(search_word in record.aliases):
                 return record
